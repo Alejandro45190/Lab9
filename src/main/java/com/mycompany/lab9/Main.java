e0a6cba9998519273cdcccafbcdff65673a0ae4a
Alejandro Rivas <r45190alejandro@gmail.com>
2021-10-14 21:42:39 -0500

    upload final product

---
+++ src/main/java/com/mycompany/lab9/Main.java
@@ -0,0 +1,25 @@
+package com.mycompany.lab9;
+
+import edu.tarleton.database.Connection;
+import edu.tarleton.database.Statement;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Main {
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
+        
+        Properties prop = new Properties();
+        try (FileInputStream in = new FileInputStream("src/main/resources/db.properties")) {
+            prop.load(in);
+            try (Connection con = new Connection(prop.getProperty("url"), prop.getProperty("user"),
+            prop.getProperty("password"))) {
+                try (Statement st = con.createStatement()) {
+                    st.executeQuery("select * from Car");
+                }
+            }
+        }
+    }
+}
